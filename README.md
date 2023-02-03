b13b6d668f1fe933f36e3399583a4df5e19069c0
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2023-02-04 03:29:51 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Curtain-e-commerce" 
---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+let swiper = new Swiper(".mySwiper", {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: true,
+  },
+});
