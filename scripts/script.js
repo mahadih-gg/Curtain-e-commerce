e88a3b27b6e2eb99ae099bb8ddd66fc615f9a08d
Mahadi92 <mahadi.hasan.937524.mh@gmail.com>
2023-02-05 03:01:23 +0600

    Responsiveness

--- scripts/script.js
+++ scripts/script.js
@@ -5,4 +5,18 @@ let swiper = new Swiper(".mySwiper", {
     delay: 3000,
     disableOnInteraction: true,
   },
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    1240: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
 });
